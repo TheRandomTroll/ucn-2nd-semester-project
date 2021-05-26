0f7569ec3ea45324320b04a6089aa343e037eb28
TheRandomTroll <mihailgerginov01@gmail.com>
2021-05-26 21:28:14 +0200

    [Add] Product CRUD unit tests

---
+++ src/test/OrderControllerTests.java
@@ -0,0 +1,13 @@
+package test;
+
+import controllers.OrderController;
+import org.junit.Before;
+
+public class OrderControllerTests {
+    private OrderController orderController;
+
+    @Before
+    public void setUp() {
+
+    }
+}
--- src/test/ProductControllerTests.java
+++ src/test/ProductControllerTests.java
@@ -1,8 +1,12 @@
 package test;
 
+import controllers.CustomerController;
 import controllers.ProductController;
+import db.DBConnection;
 import exceptions.DataAccessException;
+import models.Customer;
 import models.Product;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -44,5 +48,70 @@ public class ProductControllerTests {
         }
 
         assertNull(p);
+    }
+
+    @Test
+    public void testCreateProduct() {
+        try {
+            Product p = new Product("test", 12345678, "test description", 7.99f, 5, 50, 50);
+            int rows = this.productController.addProduct(p);
+
+            assertEquals(1, rows);
+
+            p = this.productController.findByBarcode(12345678);
+            assertEquals("test", p.getName());
+
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testUpdateProduct() {
+        try {
+            Product p = this.productController.findByBarcode(12345678);
+            p.setName("updated test");
+            int rows = this.productController.updateProduct(p);
+
+            assertEquals(1, rows);
+
+            p = this.productController.findByBarcode(12345678);
+            assertEquals("updated test", p.getName());
+
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        try {
+            Product p = this.productController.findByBarcode(12345678);
+            int rows = this.productController.deleteProduct(p.getId());
+
+            assertEquals(1, rows);
+
+            p = this.productController.findByBarcode(12345678);
+            assertNull(p);
+
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void cleanUpWhenFinish() {
+        try {
+            ProductController pc = new ProductController();
+            Product p = pc.findByBarcode(12345678);
+            int numDeleted = pc.deleteProduct(p.getId());
+            // Assert
+            assertEquals("One row deleted", 1, numDeleted);
+        } catch(Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            DBConnection.closeConnection();
+        }
+
     }
 }
